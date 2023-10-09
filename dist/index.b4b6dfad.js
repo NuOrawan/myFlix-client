120218df9b1bb9c6be13bfe858420a54934a9a90
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-10-09 16:06:41 -0700

    Update image paths in main-view.jsx and upload silence of the lambs image

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27181,39 +27181,43 @@ const MainView = ()=>{
     const [movies, setMovies] = (0, _react.useState)([
         {
             id: 1,
-            title: "Silence of the Lambs",
+            title: "The Silence of the Lambs",
             description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
             director: "Jonathan Demme",
             genre: "Thriller",
-            image: "./img/arrival.jpeg"
+            image: "img/The_Silence_of_the_Lambs.jpg"
         },
         {
             id: 2,
             title: "Harry Potter and the Sorcerer's Stone",
             description: "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
             director: "Chris Columbus",
-            genre: "Fantasy Adventure"
+            genre: "Fantasy Adventure",
+            image: "img/harry_potter_and_the_philosopher_stone.jpeg"
         },
         {
             id: 3,
             title: "Lord of the Rings:The Return of the King",
             description: "A 2003 epic fantasy adventure film directed by Peter Jackson from a screenplay by Fran Walsh",
             director: "Peter Jackson",
-            genre: "Fantasy Adventure"
+            genre: "Fantasy Adventure",
+            image: "img/the_Lord_of_the_rings_the_return_of_the_king.jpeg"
         },
         {
             id: 4,
             title: "Black Hawk Down",
             description: "Black Hawk Down is a 2001 war film directed and produced by Ridley Scott, and co-produced by Jerry Bruckheimer, from a screenplay by Ken Nolan.",
             director: "Ridley Scott",
-            genre: "Action"
+            genre: "Action",
+            image: "img/black_hawk_down.jpeg"
         },
         {
             id: 5,
             title: "Blended",
             description: "After a bad blind date, a man and woman find themselves stuck together at a resort in Africa, where their attractions grows as their respective kids.",
             director: "Frank Coraci ",
-            genre: "Comedy"
+            genre: "Comedy",
+            image: "img/blended.jpeg"
         }
     ]);
     const [selectedMovie, setSelectedMovie] = (0, _react.useState)(null);
@@ -27259,7 +27263,7 @@ const MainView = ()=>{
         columnNumber: 9
     }, undefined);
 };
-_s(MainView, "BI5Qv+oVWdOtCfHnpUbpw6ehJWE=");
+_s(MainView, "KWmX3cKtx7Gjg2a7wdRdjvYzplY=");
 _c = MainView;
 exports.default = MainView;
 var _c;
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,43 +8,43 @@ const MainView = () => {
     const [movies, setMovies] = useState([
         {
             id: 1,
-            title: "Silence of the Lambs",
+            title: "The Silence of the Lambs",
             description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
             director: "Jonathan Demme",
             genre: "Thriller",
-            image: "./img/arrival.jpeg"
+            image: "img/The_Silence_of_the_Lambs.jpg"
         },
         {
             id: 2,
             title: "Harry Potter and the Sorcerer's Stone",
             description: "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
             director: "Chris Columbus",
-            genre: "Fantasy Adventure"
-            //image: "harry_potter_and_the_philosopher_stone.jpeg"
+            genre: "Fantasy Adventure",
+            image: "img/harry_potter_and_the_philosopher_stone.jpeg"
         },
         {
             id: 3,
             title: "Lord of the Rings:The Return of the King",
             description: "A 2003 epic fantasy adventure film directed by Peter Jackson from a screenplay by Fran Walsh",
             director: "Peter Jackson",
-            genre: "Fantasy Adventure"
-            //image: "the_Lord_of_the_rings_the_return_of_the_king.jpeg"
+            genre: "Fantasy Adventure",
+            image: "img/the_Lord_of_the_rings_the_return_of_the_king.jpeg"
         },
         {
             id: 4,
             title: "Black Hawk Down",
             description: "Black Hawk Down is a 2001 war film directed and produced by Ridley Scott, and co-produced by Jerry Bruckheimer, from a screenplay by Ken Nolan.",
             director: "Ridley Scott",
-            genre: "Action"
-            //image: "black_hawk_down.jpeg"
+            genre: "Action",
+            image: "img/black_hawk_down.jpeg"
         },
         {
             id: 5,
             title: "Blended",
             description: "After a bad blind date, a man and woman find themselves stuck together at a resort in Africa, where their attractions grows as their respective kids.",
             director: "Frank Coraci ",
-            genre: "Comedy"
-            //image: "blended.jpeg"
+            genre: "Comedy",
+            image: "img/blended.jpeg"
         }
     ]);
     
